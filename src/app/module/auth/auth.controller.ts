4130b823955352629d845baa5f941348599c9c01
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-08-10 20:08:52 +0600

    feat: validate higher order function implemented

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -7,11 +7,8 @@ import { AuthService } from "./auth.service";
 import { patientRegistrationSchema } from "../../utils/zodSchemas/patientRegistrationSchema";
 
 const registerPatient = catchAsync(async (req: Request, res: Response) => {
-	const payload = patientRegistrationSchema.safeParse(req.body)
-	if(!payload.success){
-		throw new Error(payload.error.issues[0].message)
-	}
-	const result = await AuthService.registerPatient(payload.data);
+	const payload = req.body
+	const result = await AuthService.registerPatient(payload);
 
 	const { accessToken, refreshToken, user, patient } = result;
 
--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { auth } from "../../middleware/checkAuth";
 import { AuthController } from "./auth.controller";
 import { Role } from "../../../../generated/enums";
+import { validateRequest } from "../../middleware/validateRequest";
+import { patientRegistrationSchema } from "../../utils/zodSchemas/patientRegistrationSchema";
 
 const router = Router();
 
-router.post("/register", AuthController.registerPatient);
+router.post("/register", validateRequest(patientRegistrationSchema), AuthController.registerPatient);
 router.post("/login", AuthController.loginUser);
 router.get(
 	"/me",
