b1da650663a1c48fb52a25cf27bcd145360d8b10
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-08-28 16:38:43 +0600

    feat: updateSchedule, publishSchedule, deleteSchedule implemented

--- src/app/module/schedule/schedule.service.ts
+++ src/app/module/schedule/schedule.service.ts
@@ -2,10 +2,14 @@ import httpStatus from "http-status";
 import { prisma } from "../../lib/prisma";
 import { RequestUser } from "../../middleware/checkAuth";
 import { AppError } from "../../utils/AppError";
-import { ICreateSchedulePayload } from "./schedule.interface";
+import {
+  ICreateSchedulePayload,
+  IUpdateSchedulePayload,
+} from "./schedule.interface";
 import { addDays, differenceInMinutes, startOfDay } from "date-fns";
 import { IQuery } from "../../interfaces";
 import { ScheduleWhereInput } from "../../../../generated/models";
+import { ScheduleStatus } from "../../../../generated/enums";
 
 const createSchedule = async (
   payload: ICreateSchedulePayload,
@@ -248,13 +252,175 @@ const getScheduleById = async (scheduleId: string) => {
   }
 };
 
-const updateSchedule = async () => {};
-const publishSchedule = async () => {};
-const deleteSchedule = async () => {};
+const updateSchedule = async (
+  scheduleId: string,
+  payload: IUpdateSchedulePayload,
+  user: RequestUser,
+) => {
+  const doctor = await prisma.doctor.findUnique({
+    where: {
+     userId: user.userId,
+    },
+  });
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor Profile Not Found");
+  }
+  const schedule = await prisma.schedule.findUnique({
+    where: { id: scheduleId, doctorId: doctor.id },
+  });
+
+  if (!schedule || schedule.isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, "Schedule Not Found");
+  }
+
+  if (schedule.status === ScheduleStatus.PUBLISHED && schedule.availableSlots) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "Schedule once published and appointment booked cannot be updated",
+    );
+  }
+
+  // const updateData: IUpdateSchedulePayload = {}
+
+  // if(payload.meetingLink){
+  //   updateData.meetingLink = payload.meetingLink || schedule.meetingLink
+  // }
+  payload.meetingLink = payload.meetingLink || schedule.meetingLink;
+  payload.startDateTime = payload.startDateTime || schedule.startDateTime;
+  payload.endDateTime = payload.endDateTime || schedule.endDateTime;
+
+  const startOfTheDay = startOfDay(payload.startDateTime); // 28 Aug => 12.00 Am
+  const startOfNextDay = addDays(startOfTheDay, 1); // 29 Aug => 12.00 Am
+
+  const existingScheduleOnThisDate = await prisma.schedule.findFirst({
+    where: {
+      doctorId: doctor.id,
+      isDeleted: false,
+      startDateTime: {
+        gte: startOfTheDay,
+        lt: startOfNextDay,
+      },
+    },
+  });
+
+  if (existingScheduleOnThisDate) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "You Already Have A Schedule For This Date",
+    );
+  }
+
+  // duration of start and end time in minutes
+  const durationInMinutes = differenceInMinutes(
+    payload.startDateTime,
+    payload.endDateTime,
+  );
+
+  const MINUTES_ALLOCATED_PER_SLOT = 20;
+
+  const totalSlots = Math.floor(durationInMinutes / MINUTES_ALLOCATED_PER_SLOT);
+
+  const scheduleUpdate = await prisma.schedule.update({
+    where: {
+      id: schedule.id,
+    },
+    data: {
+      startDateTime: payload.startDateTime,
+      endDateTime: payload.endDateTime,
+      meetingLink: payload.meetingLink,
+      totalSlots,
+      availableSlots: totalSlots,
+      doctorId: doctor.id,
+    },
+    include: {
+      doctor: {
+        select: {
+          name: true,
+          email: true,
+          address: true,
+          contactNumber: true,
+          consultationFee: true,
+        },
+      },
+    },
+  });
+
+  return scheduleUpdate;
+};
+const publishSchedule = async (scheduleId: string, user: RequestUser) => {
+   const doctor = await prisma.doctor.findUnique({
+    where: {
+      userId: user.userId,
+    },
+  });
+
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor Profile Not Found");
+  }
+  const schedule = await prisma.schedule.findUnique({
+    where: { id: scheduleId, doctorId: doctor.id },
+  });
+
+  if (!schedule || schedule.isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, "Schedule Not Found");
+  }
+  if(schedule.status === ScheduleStatus.PUBLISHED){
+    throw new AppError(httpStatus.CONFLICT, "Schedule Is Already Published")
+  }
+  const publishedSchedule = await prisma.schedule.update({
+    where:{id: schedule.id},
+    data:{
+      status: ScheduleStatus.PUBLISHED
+    }
+  })
+
+  return publishedSchedule
+};
+const deleteSchedule = async (scheduleId: string, user: RequestUser) => {
+     const doctor = await prisma.doctor.findUnique({
+    where: {
+      userId: user.userId,
+    },
+  });
+
+  if (!doctor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Doctor Profile Not Found");
+  }
+  const schedule = await prisma.schedule.findUnique({
+    where: { id: scheduleId, doctorId: doctor.id },
+  });
+
+  if (!schedule || schedule.isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, "Schedule Not Found");
+  }
+
+    if (schedule.status === ScheduleStatus.PUBLISHED && schedule.availableSlots) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "Schedule once published and appointment booked cannot be deleted",
+    );
+  }
+
+  const deletedSchedule = await prisma.schedule.update({
+    where:{
+      id: schedule.id
+    },
+    data:{
+      isDeleted: true,
+      deletedAt: new Date()
+    }
+  })
+
+  return deletedSchedule
+  
+}
 
 export const scheduleServices = {
   createSchedule,
   getMySchedule,
   getAllSchedules,
   getScheduleById,
+  updateSchedule,
+  publishSchedule,
+  deleteSchedule,
 };
