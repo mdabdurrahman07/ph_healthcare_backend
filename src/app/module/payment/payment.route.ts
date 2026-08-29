7dfe10b5e09262f6ca55e132d202bee1559e8b23
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-08-29 20:16:50 +0600

    payment module all routes services and controller implemented

---
+++ src/app/module/payment/payment.route.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { auth } from "../../middleware/checkAuth";
+import { Role } from "../../../../generated/enums";
+import { paymentController } from "./payment.controller";
+
+const router = Router();
+
+router.get("/my-payments", auth(Role.PATIENT), paymentController.getMyPayments);
+
+router.get(
+    "/all-payments",
+    auth(Role.ADMIN, Role.SUPER_ADMIN),
+    paymentController.getAllPayments,
+);
+
+router.get(
+    "/:paymentId",
+    auth(Role.PATIENT, Role.ADMIN, Role.SUPER_ADMIN),
+    paymentController.getSinglePayment,
+);
+
+export const PaymentRoutes = router;
